611bdfd9dd1a6a30bf28a5ac5e4b8bf7dcf47341
kadir-zeren <k4dzeren@gmail.com>
2024-02-29 01:57:36 +0300

    js started

---
+++ app.js
@@ -0,0 +1,48 @@
+// console.log("local");
+
+// alert("Bu bir alert dir");
+
+// karar = confirm("Emin misiniz E/H");
+// console.log(karar);
+
+// console.warn("Bu bir uyaridir");
+
+// console.error("Eyvah hata olust..");
+
+console.log("APP js");
+const pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+const sayi1 = "3";
+console.log(typeof sayi1);
+
+const varMi = true;
+const isOpen = false;
+
+console.log(isOpen, typeof isOpen);
+
+let language = "Java";
+console.log(language, typeof language);
+
+language = "Javascript";
+console.log(language);
+
+language = true;
+console.log(language);
+
+language = 3.14;
+console.log(language);
+
+let adet;
+console.log(adet);
+
+adet = "23";
+console.log(typeof adet);
+
+{
+  let localDegisken = 5;
+  localDegisken = localDegisken + 1;
+  console.log(localDegisken);
+}
+
